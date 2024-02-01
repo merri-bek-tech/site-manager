f2041875b3bbd526872a4a5c28c70c97cc7df39d
Craig Ambrose <craig@craigambrose.com>
2024-02-01 20:28:33 +1100

    can start and stop the node much better

--- backend/src/rocket_aquadoggo/container.rs
+++ backend/src/rocket_aquadoggo/container.rs
@@ -1,4 +1,4 @@
-use std::sync::{Mutex};
+use std::sync::{Mutex, Arc};
 use aquadoggo::{Configuration, Node};
 use p2panda_rs::identity::KeyPair;
 use rocket::{serde::{Serialize, Deserialize}, tokio};
@@ -14,12 +14,12 @@ pub enum NodeStatus {
 
 #[derive(Default)]
 pub struct AquadoggoContainer {
-    pub node: Mutex<Option<Node>>,
+    pub node: Arc<Mutex<Option<Node>>>,
 }
 
 impl AquadoggoContainer {
     pub fn status(&self) -> NodeStatus {
-        let mut node_lock = self.node.lock().unwrap();
+        let node_lock = self.node.lock().unwrap();
 
         if node_lock.is_some() {
             println!("Aquadoggo: Checking status of node - it is running");
@@ -35,11 +35,8 @@ impl AquadoggoContainer {
             panic!("Aquadoggo: Node already running");
         }
 
-        let mut node_lock = self.node.lock().unwrap();
+        let node_copy = self.node.clone();
 
-        // tokio spawn
-        // This works well, but I can't take a reference to the node
-        // and put in the node lock
         tokio::spawn(async move {
             println!("Aquadoggo: Starting tokio spawn");
 
@@ -47,37 +44,59 @@ impl AquadoggoContainer {
             let node = Node::start(key_pair, config).await;
             println!("Aquadoggo: Node started");
 
-            node.on_exit().await;
-            println!("Aquadoggo: Node exited");
+            // assign the node to the container
+            let mut node_lock = node_copy.lock().unwrap();
+            *node_lock = Some(node);
 
-            node.shutdown().await;
-            println!("Aquadoggo: Node shutdown");
+            // // take another node copy
+            // let node_copy = node_copy.clone();
+
+            // // wait for the node to exit
+            // let node_lock = node_copy.lock().unwrap();
+            // node.on_exit().await;
+            // println!("Aquadoggo: Node exited");
+
+            // node.shutdown().await;
+            // println!("Aquadoggo: Node shutdown");
         });
     }
 
     pub async fn shutdown_node(&self) {
-        // If the node is running, we want to shut it down
-        // then clear the node from the container
-    }
-
-    fn clear_node(&self) {
-        println!("Aquadoggo: Clearing node");
-        self.set_node(None);
-    }
-
-    fn set_node(&self, maybe_node: Option<Node>) {
+        // Take the Node out of self
         let mut node_lock = self.node.lock().unwrap();
-        //*node_lock = maybe_node;
+        let node = node_lock.take();
 
-        if maybe_node.is_some() {
-            println!("Aquadoggo: Setting node");
-            node_lock.replace(maybe_node.unwrap());
+        // shutdown the node
+        if node.is_some() {
+            println!("Aquadoggo: Shutting down node");
+
+            // spawn a tokio thread to shutdown the node
+            tokio::spawn(async move {
+                node.unwrap().shutdown().await;
+                println!("Aquadoggo: Node shutdown");
+            });
         } else {
-            println!("Aquadoggo: Clearing node");
-            node_lock.take();
+            println!("Aquadoggo: Node is already offline");
         }
-
     }
+
+    // fn clear_node(&self) {
+    //     println!("Aquadoggo: Clearing node");
+    //     self.set_node(None);
+    // }
+
+    // fn set_node(&self, maybe_node: Option<Node>) {
+    //     let mut node_lock = self.node.lock().unwrap();
+    //     //*node_lock = maybe_node;
+
+    //     if maybe_node.is_some() {
+    //         println!("Aquadoggo: Setting node");
+    //         node_lock.replace(maybe_node.unwrap());
+    //     } else {
+    //         println!("Aquadoggo: Clearing node");
+    //         node_lock.take();
+    //     }
+    // }
 
     // fn get_node(&self) -> Option<Node> {
     //     // let mut node_lock = self.node.lock().unwrap();
