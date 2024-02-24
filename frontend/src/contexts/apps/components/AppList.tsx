e3b4e1ace21b3dfb60a9c73caa57250cfb83cdf4
Craig Ambrose <craig@craigambrose.com>
2024-02-24 17:43:50 +1100

    mocked up app list

--- frontend/src/contexts/apps/components/AppList.tsx
+++ frontend/src/contexts/apps/components/AppList.tsx
@@ -1,51 +1,34 @@
 import {
   TableContainer,
   Table,
-  TableCaption,
   Thead,
   Tr,
   Th,
   Tbody,
   Td,
-  Tfoot,
+  Link,
 } from "@chakra-ui/react"
 
 export default function AppList() {
   return (
     <TableContainer>
       <Table variant="simple">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+            <Th>Name</Th>
+            <Th>Path</Th>
+            <Th>Version</Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
-          </Tr>
-          <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td isNumeric>0.91444</Td>
+            <Td>Scrib</Td>
+            <Td>
+              <Link href="/books">/books</Link>
+            </Td>
+            <Td>0.1.11</Td>
           </Tr>
         </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
-          </Tr>
-        </Tfoot>
       </Table>
     </TableContainer>
   )
